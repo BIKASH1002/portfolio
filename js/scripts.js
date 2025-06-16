4f585f3957d699c88d57d27658f74b50769acbd5
BIKASH CHANDRA SAHOO <bikashsahoo10896@gmail.com>
2025-06-16 12:28:56 +0530

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }, 250);
       }, 500);
     }
-    // Updated counters: Publications=2, Projects=21+ and Certifications=16
+
     flipCounter("papers", 3);
     flipCounter("projects", 21, true);
     flipCounter("certifications", 16);
